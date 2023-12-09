080051662badf06916ecd5f629bd5033bea5c30f
cheturm <122756239+cheturm@users.noreply.github.com>
2023-12-08 19:36:45 -0600

    removing unused imports

--- src/main/java/com/potato/jpahibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/potato/jpahibernate/course/CourseCommandLineRunner.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.potato.jpahibernate.course.jpa.CourseJpaRepo;
 import com.potato.jpahibernate.course.springdatajpa.CourseSpringDataRepo;
 
 @Component
